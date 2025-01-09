2e61f0c397c32c5ce813a7b7b0ae68f212ec4983
Olamilekan Kareem <olamilekankareem717@gmail.com>
2025-01-09 04:44:06 +0100

    learnt functions

---
+++ functions.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    return num + 2;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpuser(name, email, password, isPaid) {
+}
+var loginUser = function (name, email, isPaid) { };
+signUpuser("Ola", "ola@gmail.com", "password", true);
+addTwo(5);
+loginUser("Ola", "ola@gmail.com", false);
---
+++ functions.ts
@@ -0,0 +1,38 @@
+function addTwo(num : number): number{
+    return num + 2;
+}
+
+function getUpper(val: string){
+    return val.toUpperCase();
+}
+
+function signUpuser(name: string, email: string, password: string, isPaid: boolean){
+}
+
+let loginUser = (name: string, email : string, isPaid: false) =>{}
+
+signUpuser("Ola", "ola@gmail.com", "password", true)
+addTwo(5)
+loginUser("Ola", "ola@gmail.com", false)
+
+function getValue(myVal : number)  {
+    if (myVal > 10) {
+        return true
+    } else {
+        return "200 ok"
+    }
+}
+
+const getHello = (s: string): string => {
+    return ""
+}
+
+const heros = ["thor", "spiderman", "iromnan"]
+
+// ifthe heros array is a number, then the type of each hero will be a number"
+
+heros.map(hero =>{
+    return `hero is ${hero}`
+})
+
+export {}
