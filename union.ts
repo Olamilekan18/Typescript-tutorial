4042968ce625ee4a2b6097cbecf5dc5e272263ea
Olamilekan Kareem <olamilekankareem717@gmail.com>
2025-01-11 04:13:00 +0100

    learnt unions

--- union.ts
+++ union.ts
@@ -29,5 +29,20 @@ function getDbId(id: number | string){
     console.log(`DB ID: ${id}`) 
 }
 
+// array
+
+const data: number[] = [1,2,3,4]
+
+const data2 : Array<string> = ["1", "2", "3", "4"]
+
+const data3 : [number, string] = [1, "2"] 
+
+// we cant use [number] | [string], that means it's either a string array or number array
+// we can also use any
+
+let pi:3.14 = 3.14;
+
+let seatAllotment : "aisle" | "middle" | "window"
+
 getDbId(3)
 getDbId("3")
