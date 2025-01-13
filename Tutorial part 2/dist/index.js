8afb1f948a9a52b98c28cc232aa2f448eeebe9f1
Olamilekan Kareem <olamilekankareem717@gmail.com>
2025-01-14 00:18:04 +0100

    learnt classes

--- Tutorial part 2/dist/index.js
+++ Tutorial part 2/dist/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("hello world!");
+console.log("hello world, lets see how the world goes this year");
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.name = name;
+        this.age = age;
+    }
+}
+const Ola = new User("Joy", 14);
--- Tutorial part 2/src/index.ts
+++ Tutorial part 2/src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello world!");
+console.log("hello world, lets see how the world goes this year");
+
+class User {
+    constructor(public name: string, public age: number) {
+        this.name = name
+        this.age = age
+    
+    }
+}
+const Ola = new User( "Joy", 14);
