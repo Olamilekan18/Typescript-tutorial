50156bf0f9fec58307fb4ee9e709307bb573f1ee
Olamilekan Kareem <olamilekankareem717@gmail.com>
2025-01-15 23:50:26 +0100

    learnt private and public methods

--- Tutorial part 2/dist/index.js
+++ Tutorial part 2/dist/index.js
@@ -1,13 +1,30 @@
 "use strict";
 console.log("hello world, lets see how the world goes this year");
 class User {
+    //private makes it unaccessible after the user class
     constructor(name, age) {
         this.name = name;
         this.age = age;
+        this._courseCount = 1;
         // you can also use readonly to make any property readonly
         this.city = "";
+        this.id = "1a112";
         this.name = name;
         this.age = age;
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get getAppleEmail() {
+        return `${this.name}.${this.id}@gmail.com`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(value) {
+        if (value > 0 && value <= 10) {
+            this._courseCount = value;
+        }
     }
 }
 const Ola = new User("Joy", 14);
--- Tutorial part 2/src/index.ts
+++ Tutorial part 2/src/index.ts
@@ -1,13 +1,35 @@
 console.log("hello world, lets see how the world goes this year");
 
-class User {
+class User {    
+    private _courseCount = 1;
+
     // you can also use readonly to make any property readonly
     city: string = ""
+    private readonly id: string = "1a112"
+    //private makes it unaccessible after the user class
     constructor(public name: string, public age: number) {
         this.name = name
         this.age = age
     
     }
+
+    private deleteToken(){
+        console.log("Token deleted")
+    }
+
+    get getAppleEmail(): string{
+        return `${this.name}.${this.id}@gmail.com`
+    }
+    get courseCount(): number{
+        return this._courseCount
+    }
+
+    set courseCount(value: number){
+        if(value > 0 && value <= 10){
+            this._courseCount = value
+        }
+    }
+    //you can't make CourseCount :number or any other type
 }
 const Ola = new User( "Joy", 14);
-Ola.city = "Ife"
+Ola.city = "Ife" 
